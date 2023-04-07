78a75d6b14a602f0e63536f93b4638afb5d25008
Alexander <balearic.k@gmail.com>
2023-04-07 14:31:53 +0600

    add bezierPath

--- WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
+++ WorkoutApp/Controllers/Session/Views/Timer/TimerView.swift
@@ -11,7 +11,7 @@ final class TimerView: WABaseInfoView {
     
     private let progressView: ProgressView = {
         let view = ProgressView()
-        view.drawProgress(with: 0)
+        view.drawProgress(with: 1)
         return view
     }()
 }
@@ -32,12 +32,13 @@ extension TimerView {
             progressView.topAnchor.constraint(equalTo: topAnchor, constant: 40),
             progressView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
             progressView.heightAnchor.constraint(equalTo: progressView.widthAnchor),
+            
         ])
     }
     
     override func configureAppearance() {
         super.configureAppearance()
-        progressView.backgroundColor = .red
-
+        
+        
     }
 }
